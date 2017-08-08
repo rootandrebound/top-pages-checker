75d36bbda8b8e502948fd96954eddc560c2f60e9
ashleyvernon <avernon526@gmail.com>
2017-08-07 19:29:43 -0700

    adds json to example environmental variables

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ It is intended to be run about once per day.
 """
 import os
 import requests
+import json
+from pprint import pprint
 
 
 def main():
+    GOOGLE_API_JSON = os.environ.get("GOOGLE_API_JSON")
+    google_api_credentials = json.loads(GOOGLE_API_JSON)
+    pprint(google_api_credentials)
     # connect to google analytics reporter API
     print("Connecting to Google Analytics Core Reporting API")
     # pull report
