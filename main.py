94e8510cf52af2719dc44635a2e302ef64903f44
Ben Golder <bgolder@codeforamerica.org>
2017-08-23 00:24:32 -0700

    functioning script

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ and then updates a json file in a github repo with the results.
 It is intended to be run about once per day.
 """
 import os
+import io
 import requests
 import json
 from pprint import pprint
 import honcho
+import boto3
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 VIEW_ID = '154632053'
+GOOGLE_API_JSON = os.environ.get("GOOGLE_API_JSON")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+JSON_FILE_NAME = 'top_pages.json'
+NUMBER_OF_TOP_PAGES = 10
 
 
 def initialize_analyticsreporting(api_json_data):
@@ -28,16 +34,29 @@ def initialize_analyticsreporting(api_json_data):
   analytics = build('analyticsreporting', 'v4', credentials=credentials)
   return analytics
 
+
+def parse_top_page(raw_ga_dict):
+    title, url = raw_ga_dict['dimensions']
+    return dict(
+        url=url,
+        title=title.replace(' - Roadmap to Reentry', ''))
+
+
 def main():
-    GOOGLE_API_JSON = os.environ.get("GOOGLE_API_JSON")
+    # create bucket if it doesnt exist
+    print("Initializing connection to Amazon S3")
+    s3 = boto3.client('s3')
+    print("Ensuring that s3 bucket '{}' exists".format(S3_BUCKET_NAME))
+    bucket_exists = S3_BUCKET_NAME in [
+        bucket['Name'] for bucket in s3.list_buckets()['Buckets']]
+    if not bucket_exists:
+        print("Creating s3 bucket '{}'".format(S3_BUCKET_NAME))
+        s3.create_bucket(Bucket=S3_BUCKET_NAME)
+    print("Connecting to Google Analytics Core Reporting API")
     google_api_credentials = json.loads(
         GOOGLE_API_JSON.translate(str.maketrans({"\n": r"\n"})))
-    # connect to google analytics reporter API
-    print("Connecting to Google Analytics Core Reporting API")
-    # import ipdb; ipdb.set_trace()
     analytics = initialize_analyticsreporting(google_api_credentials)
-    # pull report
-    print("Pulling information about most visited pages")
+    print("Requesting list of most visited pages")
     report_params = {
         'viewId': VIEW_ID,
         'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
@@ -53,14 +72,21 @@ def main():
             {"fieldName": "ga:pageviews", "sortOrder": "DESCENDING"}]
     }
     response = analytics.reports().batchGet(
-      body={'reportRequests': [report_params]}).execute()
-    pprint(response)
-    # print_response(response)
-    # connect to github
-    print("Connecting to github")
-    # write a file??
-    print("Overwriting contents of 'output/js/top_pages.json'")
-
+        body={'reportRequests': [report_params]}).execute()
+    top_pages = [
+        parse_top_page(row)
+        for row in response['reports'][0]['data']['rows'][:10]]
+    print("Received new list of top pages:")
+    pprint(top_pages)
+    top_pages_json = json.dumps(top_pages)
+    top_pages_json_bytes = top_pages_json.encode('utf-8')
+    json_file = io.BytesIO(top_pages_json_bytes)
+    print("Saving top pages to '{}'".format(JSON_FILE_NAME))
+    s3.upload_fileobj(json_file, S3_BUCKET_NAME, JSON_FILE_NAME)
+    print("Allowing public access to '{}'".format(JSON_FILE_NAME))
+    s3.put_object_acl(
+        ACL='public-read', Bucket=S3_BUCKET_NAME, Key=JSON_FILE_NAME)
+    print("Complete.")
 
 if __name__ == '__main__':
     main()
